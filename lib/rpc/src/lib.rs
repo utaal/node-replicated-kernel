3fa337336e86445c55447033c5463054c71cac7f
Erika Hunhoff <hunhoff.erika@gmail.com>
2021-07-02 10:54:06 -0700

    Added API code in

--- lib/rpc/src/lib.rs
+++ lib/rpc/src/lib.rs
@@ -1,10 +1,15 @@
 #![no_std]
 
 #[macro_use]
-//extern crate abomonation;
+extern crate abomonation;
 extern crate alloc;
-//extern crate lazy_static;
-//extern crate smoltcp;
+extern crate lazy_static;
+extern crate smoltcp;
+
+pub mod cluster_api;
+pub mod rpc;
+pub mod rpc_api;
+//pub mod tcp_client;
 
 #[cfg(test)]
 mod tests {
